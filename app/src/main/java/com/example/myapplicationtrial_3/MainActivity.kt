ff9d64a42fb8c70c5ab5aeaa0ee5407663e14271
adwinsimanjuntak <adwinwellmark@gmail.com>
2021-06-05 19:25:27 +0700

    second commit

--- app/src/main/java/com/example/myapplicationtrial_3/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtrial_3/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.myapplicationtrial_3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//first commit
+//second commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
